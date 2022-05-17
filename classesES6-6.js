7080a9593a316e9d48fe1a6fd3bb41c4bda879a0
Harris <acyclicgraphs@gmail.com>
2022-05-17 12:14:52 -0400

    Update classesES6-6.js

--- classesES6-6.js
+++ classesES6-6.js
@@ -9,9 +9,24 @@ getSummary() {
  return `${this.title} was written by $
     {this.author} in ${this.year}`;
   }
+  
+  getAge() {
+   const years = new Data().getFullYear - this.year;
+   return `${this.title} is ${years} years old`;
 }
+
+revise(newYear) {
+  this.year = newYear;
+  this.revised = true;
+  }
+}
+
 
 // Instantiate Object
 const book1 = new Book('Book One', 'John Doe', '2013');
 
-console.log(book1);
+// console.log(book1);
+// book1.revise('2018');
+// console.log(book1);
+
+console.log(Book.topBookStore());
