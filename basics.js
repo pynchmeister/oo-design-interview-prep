a51c05b51a1dd1c94e52ff6725a364c6375fa814
Harris <acyclicgraphs@gmail.com>
2022-05-17 10:51:32 -0400

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,13 @@
+// Object Literal
+const book1 = {
+  title: 'Book One',
+  author: 'John Doe',
+  year: '2013',
+  getSummary: function() {
+    return `${this.title} was written by $
+    {this.author} in ${this.year}`;
+  }
+};
+
+console.log(book1.title);
+
